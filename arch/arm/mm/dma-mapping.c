be99c2605b2edeb07e211befe52ed1214cf39ef1
Marek Szyprowski <m.szyprowski@samsung.com>
2014-01-16 15:39:17 +0100

    ARM: dma-mapping: fix GFP_ATOMIC macro usage
    commit 10c8562f932d89c030083e15f9279971ed637136 upstream.
    
    GFP_ATOMIC is not a single gfp flag, but a macro which expands to the other
    flags and LACK of __GFP_WAIT flag. To check if caller wanted to perform an
    atomic allocation, the code must test __GFP_WAIT flag presence. This patch
    fixes the issue introduced in v3.6-rc5
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -1696,9 +1696,8 @@ static void *arm_iommu_alloc_attrs(struct device *dev, size_t size,
 
 	size = PAGE_ALIGN(size);
 
-	if (gfp & GFP_ATOMIC)
-
-		return __iommu_alloc_atomic(dev, size, handle, attrs);
+	if (!(gfp & __GFP_WAIT))
+		return __iommu_alloc_atomic(dev, size, handle);
 
 	pages = __iommu_alloc_buffer(dev, size, gfp, attrs);
 	if (!pages)
